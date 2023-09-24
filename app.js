66234e245917c74621999c1ed48d1b097181ffb5
Kaushik paul <kaushikpul29999@gmail.com>
2023-09-24 17:11:21 +0530

    update

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ searchicon1.addEventListener('click', function()
     search1.style.display ='flex';
     searchicon1.style.display='none';
 })
+
+
+
 const searchicon2=document.querySelector('#searchicon2');
 const srchicon2 = document.querySelector('#srchicon2');
 const search2= document.querySelector('#searchinput2');
@@ -15,4 +18,21 @@ searchicon2.addEventListener('click', function()
 {
     search2.style.display ='flex';
     searchicon2.style.display='none';
+})
+const bar =document.querySelector('.fa-bars');
+const  cross =document.querySelector('#hdcross');
+const headerbar =document.querySelector('.headerbar');
+
+bar.addEventListener('click', function()
+{
+    setTimeout(() => {
+        cross.style.display = 'block';
+    }, 200);
+    headerbar.style.right ='0%'
+})
+
+cross.addEventListener('click',function()
+{
+    cross.style.display='none';
+    headerbar.style.right='-100%'
 })
